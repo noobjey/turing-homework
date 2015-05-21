fc4b5ed0b24decc143f71f5c22a24df7a4f31fd9
Jason Wright <noobjey@gmail.com>
2015-05-21 06:56:41 -0600

    level3 complete

--- challenges/state_capitals.rb
+++ challenges/state_capitals.rb
@@ -15,6 +15,10 @@ puts capitals[states[name]]
 # Level 2: Handle the case when a state's information is not known by returning "Unknown"
 unknown_info = "Missouri"
 capital = capitals[states[unknown_info]]
+
 puts capital.nil? ? "Unknown" : capital
 
 # Level 3: Now let's go the other way. Given a capital name ("Denver"), return the state name for which it is the capital ("Colorado")
+capital = "Denver"
+
+puts states.invert[capitals.invert[capital]]
