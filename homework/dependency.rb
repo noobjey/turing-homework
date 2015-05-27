0fdd6bcacf237b04720298415acaedb1e4c82798
Jason Wright <noobjey@gmail.com>
2015-05-27 14:56:35 -0600

    first dependency try

--- homework/dependency.rb
+++ homework/dependency.rb
@@ -5,26 +5,61 @@
 #Then, let's do an experiment with dependency injection. Check out this usage:
 #
 
-class ScreenPrinter
+class Fibber
+  attr_accessor :printer
 
+  def initialize(printer)
+    @printer = printer
+  end
+
+  def generate(quantity)
+    if (quantity.is_a? String) || (quantity < 1)
+      0
+    elsif quantity < 3
+      1
+    else
+      generate(quantity - 1) + generate(quantity - 2)
+    end
+  end
+
+  def all_fib_up_to(quantity)
+    all_fibs = (0..quantity).map do |number|
+      generate(number)
+    end
+    all_fibs
+  end
+
+  def print(quantity)
+    all_fib_up_to(quantity).each do |number|
+      printer.print(number)
+    end
+  end
 end
 
-class Fibber
-
-
+class ScreenPrinter
+  def print(value)
+    printf "#{value} "
+  end
 end
 
 class FilePrinter
+  def initialize(file_name)
+    @file_name = file_name
+    @file = File.open file_name, 'a+'
+  end
 
+  def print(value)
+    @file.write "#{value} "
+  end
 end
 
 
 screen_printer = ScreenPrinter.new
-# fibber = Fibber.new(screen_printer)
-# fibber.print(10) # => prints the first 10 numbers of the fibonacci sequence to
+fibber = Fibber.new(screen_printer)
+fibber.print(10) # => prints the first 10 numbers of the fibonacci sequence to
 #                  #    the screen separated by a single space
-# file_printer = FilePrinter.new("output.txt")
-# fibber = Fibber.new(file_printer)
-# fibber.print(10) # => prints the first 10 number of the fibonacci sequence to
+file_printer = FilePrinter.new("output.txt")
+fibber = Fibber.new(file_printer)
+fibber.print(10) # => prints the first 10 number of the fibonacci sequence to
 #                  #    a file named 'output.txt' separated by a single space
 # Build Fibber, ScreenPrinter, and FilePrinter classes to make that work.
