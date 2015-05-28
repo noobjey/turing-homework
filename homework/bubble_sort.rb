2b42973a64423c1b9425ac364b0c06ab64eb9ac9
Jason Wright <noobjey@gmail.com>
2015-05-28 16:30:25 -0600

    initial checkin

---
+++ homework/bubble_sort.rb
@@ -0,0 +1,13 @@
+# https://github.com/turingschool/challenges/blob/master/bubble_sort.markdown
+
+# Challenge 1: Without Custom Classes
+#
+# Write an implementation of bubble sort that does not use any custom classes. You'll likely want to use methods and will surely needs arrays and a while loop.
+#
+# In addition to writing an implementation following the template below, answer the following questions:
+#
+# Given the numbers 0 through 5, what would be the worst case scenario for bubble sort (aka, what order would necessitate the most swaps)?
+# How many swaps would that worst case take?
+# The example above took 21 iterations to get to a result. Can you tweak the algorithm so that it takes the same
+# number of swaps (7) but fewer total operations?
+
